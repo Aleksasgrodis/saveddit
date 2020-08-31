d9d666bcfd1602c1e79750fd39ee4c12c0826177
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-08-31 19:19:31 +0200

    Now able to retrieve bearer token with serverless function

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,17 +1,25 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect } from 'react';
 import URLParse from 'url-parse';
 import { UserContext } from '../context/UserContext';
 
 const Dashboard = () => {
-  const url = new URLParse(window.location, true)
-  const { user, setUser } = useContext(UserContext)
+  const url = new URLParse(window.location, true);
+  const { user, setUser } = useContext(UserContext);
   const seed = localStorage.getItem('seed');
   useEffect(() => {
+    const fetchUserToken = code => {
+      fetch(`/api/token?code=${code}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    };
+
     if (url && url.query.state === seed) {
-      setUser({...user, code: url.query.code})
+      setUser({ ...user, code: url.query.code });
+      fetchUserToken(url.query.code);
     }
-  }, [])
-  
+  }, []);
+
   return (
     <div className="dashboard">
       <h2>welcome</h2>
@@ -19,4 +27,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard
+export default Dashboard;
