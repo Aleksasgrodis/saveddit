646716a986e9db39600ea21f65e6b754c1d26945
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-10-27 22:53:51 +0100

    Back button in searchbar now goes back one page in history.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Search() {
   // takes in props or array to search in and bounces results to page
+  const history = useHistory();
   return (
     <div className="mt-5">
-      <button class="bg-gray-500 shadow-inner hover:bg-gray-700 text-white text-2xl font-bold py-1 px-4 rounded-lg mr-5 focus:outline-none focus:shadow-outline">
+      <button onClick={() => history.go(-1)} className="bg-gray-500 shadow-inner hover:bg-gray-700 text-white text-2xl font-bold py-1 px-4 rounded-lg mr-5 focus:outline-none focus:shadow-outline">
         ←
       </button>
       <input type="text" className="py-1 shadow-inner px-4 bg-gray-500 text-white text-2xl rounded-lg placeholder-white focus:outline-none focus:shadow-outline" placeholder="Search" />
