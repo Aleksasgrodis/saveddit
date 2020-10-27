8cf6f428724640cb57473532ac8f3f635cfcf461
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-10-27 22:49:21 +0100

    Styling improvements to searchbar.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 function Search() {
   // takes in props or array to search in and bounces results to page
   return (
-    <div>
-      <input type="text" placeholder="Search"/>
+    <div className="mt-5">
+      <button class="bg-gray-500 shadow-inner hover:bg-gray-700 text-white text-2xl font-bold py-1 px-4 rounded-lg mr-5 focus:outline-none focus:shadow-outline">
+        ←
+      </button>
+      <input type="text" className="py-1 shadow-inner px-4 bg-gray-500 text-white text-2xl rounded-lg placeholder-white focus:outline-none focus:shadow-outline" placeholder="Search" />
     </div>
   );
 }
--- src/components/SubredditListItem.jsx
+++ src/components/SubredditListItem.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 function SubredditListItem({ title, hits }) {
   return (
     <NavLink
-      className="flex h-16 rounded border-2 border-gray-300 m-2"
+      className="flex h-16 rounded border-2 border-gray-300 mr-4 mb-4"
       to={`/dashboard/subreddits/${title}`}
     >
       <div className="pl-6 pr-6 self-center w-full font-medium xl:text-xl lg:text-md text-center text-gray-800">{title}</div>
