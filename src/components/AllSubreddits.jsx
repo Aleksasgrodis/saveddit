7e79ab4906f9c1642b85d750077e5910d599a89b
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-11-01 15:38:19 +0100

    Added a simple search feature to find subreddit by contents of title.

--- src/components/AllSubreddits.jsx
+++ src/components/AllSubreddits.jsx
@@ -1,5 +1,5 @@
 import { create } from 'lodash';
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavHashLink } from 'react-router-hash-link';
 import { createSelector } from 'reselect';
@@ -12,10 +12,15 @@ const subredditSelector = createSelector(linksSelector, links =>
 );
 
 function AllSubreddits() {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResults, setSearchResults] = useState(null);
+  const search = { searchValue, setSearchValue };
   const duplicateSubreddits = useSelector(subredditSelector);
   const subreddits = [...new Set(duplicateSubreddits)].sort((a, b) =>
     a.localeCompare(b),
   );
+  const copy = useMemo(() => [...duplicateSubreddits], [duplicateSubreddits]);
+
   const alphabet = [
     'A',
     'B',
@@ -49,20 +54,64 @@ function AllSubreddits() {
     [...subreddits.filter(link => link.toUpperCase().charAt(0) === letter)],
   ]);
   useEffect(() => {
-    
-    console.log(window.location);
-  }, [])
+    const alphabetEffect = [
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O',
+      'P',
+      'Q',
+      'R',
+      'S',
+      'T',
+      'U',
+      'V',
+      'W',
+      'X',
+      'Y',
+      'Z',
+    ];
+    if (copy) {
+      const sortedSubreddits = [...new Set(copy)].sort((a, b) =>
+        a.localeCompare(b),
+      );
+      setSearchResults(
+        alphabetEffect.map(letter => [
+          letter,
+          [
+            ...sortedSubreddits
+              .filter(sub =>
+                sub.toLowerCase().includes(searchValue.toLowerCase()),
+              )
+              .filter(link => link.toUpperCase().charAt(0) === letter),
+          ],
+        ]),
+      );
+    }
+  }, [searchValue, copy]);
+  console.log(searchResults);
   return (
     <div className="flex flex-col">
       <div className="fixed">
-        <Search />
+        <Search {...search} />
       </div>
       {/* {subreddits
           ? subreddits.map(sub => <SubredditListItem key={sub} title={sub} />)
           : null} */}
       <div className="flex">
         <div className="mt-16">
-          {sortedByLetter
+          {sortedByLetter && !searchValue.length
             ? sortedByLetter.map(letter => {
                 if (letter[1].length) {
                   return (
@@ -81,15 +130,32 @@ function AllSubreddits() {
                   );
                 } else return null;
               })
-            : null}
+            : searchResults.map(letter => {
+              if (letter[1].length) {
+                return (
+                  <section
+                    id={`section-${letter[0].toLowerCase()}`}
+                    className="w-11/12 flex flex-col flex-wrap"
+                    key={letter[0]}
+                  >
+                    <h2 className="font-bold text-4xl">{letter[0]}</h2>
+                    <div className="flex flex-wrap">
+                      {letter[1].map(sub => (
+                        <SubredditListItem key={sub} title={sub} />
+                      ))}
+                    </div>
+                  </section>
+                );
+              } else return null;
+            })}
         </div>
         <div className="fixed right-0 inset-y-0 mr-16">
           <div className="flex flex-col h-full justify-center items-center">
             {sortedByLetter.map(letter => {
               return letter[1].length ? (
                 <NavHashLink
-                key={letter[0]}
-                className="font-bold text-gray-400 hover:text-gray-900"
+                  key={letter[0]}
+                  className="font-bold text-gray-400 hover:text-gray-900"
                   to={`/dashboard/subreddits#section-${letter[0].toLowerCase()}`}
                   smooth
                   activeClassName="text-orange-600 text-xl font-bolder"
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-function Search() {
+function Search({ searchValue, setSearchValue }) {
   // takes in props or array to search in and bounces results to page
   const history = useHistory();
   return (
     <div className="mt-5 opacity-25 hover:opacity-100 focus:opacity-100">
-      <button onClick={() => history.go(-1)} className="bg-gray-500 shadow-inner hover:bg-gray-700 text-white text-2xl font-bold py-1 px-4 rounded-lg mr-5 focus:outline-none focus:shadow-outline">
+      <button
+        onClick={() => history.go(-1)}
+        className="bg-gray-500 shadow-inner hover:bg-gray-700 text-white text-2xl font-bold py-1 px-4 rounded-lg mr-5 focus:outline-none focus:shadow-outline"
+      >
         ←
       </button>
-      <input type="text" className="focus:opacity-100 py-1 shadow-inner px-4 bg-gray-500 text-white text-2xl rounded-lg placeholder-white focus:outline-none focus:shadow-outline" placeholder="Search" />
+      <input
+        type="text"
+        value={searchValue}
+        onChange={e => setSearchValue(e.target.value)}
+        className="focus:opacity-100 py-1 shadow-inner px-4 bg-gray-500 text-white text-2xl rounded-lg placeholder-white focus:outline-none focus:shadow-outline"
+        placeholder="Search"
+      />
     </div>
   );
 }
