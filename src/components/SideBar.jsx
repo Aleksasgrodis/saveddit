77fb43cf6c829769b1461e8fe394a033b1010f60
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-11-01 17:17:48 +0100

    Styling improvements to sidebar route navigation.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -14,14 +14,14 @@ function SideBar() {
     <div className="flex flex-col justify-around h-screen w-full">
       <div><UserInfo /></div>
       <div className="flex flex-col justify-center">
-        <NavLink to="/dashboard/all">All</NavLink>
-        <NavLink to="/dashboard/subreddits">Subreddits</NavLink>
-        <NavLink to="/dashboard/nsfw">NSFW</NavLink>
+        <NavLink className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow border rounded p-2 mb-2 text-center" activeClassName="font-bold text-blue-600" to="/dashboard/all">All</NavLink>
+        <NavLink className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow border rounded p-2 mb-2 text-center" activeClassName="font-bold text-blue-600" to="/dashboard/subreddits">Subreddits</NavLink>
+        <NavLink className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow border rounded p-2 mb-2 text-center" activeClassName="font-bold text-blue-600" to="/dashboard/nsfw">NSFW</NavLink>
       </div>
       <div className="flex flex-col">
         <button
           type="button"
-          className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+          className="mb-2 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
         >
           Refresh
         </button>
