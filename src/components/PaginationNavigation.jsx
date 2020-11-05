56c36d1907d59d6dd10163293aed35d31724b65f
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-11-05 15:47:15 +0100

    Quick fix to issue where sub 10 page pagination navigation would not clearly indicate the page currently being viewed.

--- src/components/PaginationNavigation.jsx
+++ src/components/PaginationNavigation.jsx
@@ -30,6 +30,8 @@ function PaginationNavigation({ action, total, currentPage }) {
                   onClick={() => dispatch(action({ page: i + 1 }))}
                   className={`${
                     currentPage === total ? 'cursor-not-allowed' : ''
+                  } ${
+                    i + 1 === currentPage ? 'text-orange-600' : ''
                   } -ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150`}
                 >
                   {i + 1}
