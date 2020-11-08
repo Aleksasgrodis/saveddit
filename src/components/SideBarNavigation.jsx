f0f66716e721f53ea234be81808a526d2d9d96bf
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-11-08 19:24:17 +0100

    Minor styling improvements to sidebar router navigation.

--- src/components/SideBarNavigation.jsx
+++ src/components/SideBarNavigation.jsx
@@ -5,22 +5,22 @@ function SideBarNavigation() {
   return (
     <nav className="flex flex-col justify-center">
         <NavLink
-          className="text-center font-semibold text-md block border border-white rounded hover:border-gray-200 hover:bg-gray-200 py-2 px-4 mb-2"
-          activeClassName="text-white text-lg font-extrabold block border border-blue-500 bg-blue-500 hover:bg-blue-700"
+          className="text-center font-semibold text-md block border border-gray-200 bg-gray-200 rounded hover:border-gray-400 hover:bg-gray-300 py-2 px-4 mb-2"
+          activeClassName="text-white text-lg font-extrabold block border border-blue-500 bg-blue-500 hover:bg-blue-600"
           to="/dashboard/all"
         >
           All
         </NavLink>
         <NavLink
-          className="text-center font-semibold block border border-white rounded hover:border-gray-200 hover:bg-gray-200 py-2 px-4 mb-2"
-          activeClassName="text-white text-lg font-extrabold block border border-blue-500 bg-blue-500 hover:bg-blue-700"
+          className="text-center font-semibold block border border-gray-200 bg-gray-200 rounded hover:border-gray-400 hover:bg-gray-300 py-2 px-4 mb-2"
+          activeClassName="text-white text-lg font-extrabold block border border-blue-500 bg-blue-500 hover:bg-blue-600"
           to="/dashboard/subreddits"
         >
           Subreddits
         </NavLink>
         <NavLink
-          className="text-center font-semibold block border border-white rounded hover:border-gray-200 hover:bg-gray-200 py-2 px-4 mb-2"
-          activeClassName="text-white text-lg font-extrabold block border border-blue-500 bg-blue-500 hover:bg-blue-700"
+          className="text-center font-semibold block border border-gray-200 bg-gray-200 rounded hover:border-gray-400 hover:bg-gray-300 py-2 px-4 mb-2"
+          activeClassName="text-white text-lg font-extrabold block border border-blue-500 bg-blue-500 hover:bg-blue-600"
           to="/dashboard/nsfw"
         >
           NSFW
