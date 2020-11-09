18c79275cf5558a73dfe1caf95eb003893dea8ef
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-11-09 19:39:10 +0100

    Remove obsolete files.

--- src/components/FilterByAll.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function FilterByAll() {
-  return (
-    <div>show all links here with sorting and few filtering abilities</div>
-  );
-}
-
-export default FilterByAll;
