ff3b3c4f11f63091833b55914d23bb72f3671ac5
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-11-09 22:29:00 +0100

    Rewrote authorize cloud function to async await logic.

--- api/authorize.js
+++ api/authorize.js
@@ -1,12 +1,18 @@
 const { default: fetch } = require('node-fetch');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const seed = req.body.seed;
   const redirect_uri =
     process.env.NODE_ENV === 'production'
       ? 'https://saveddit.vercel.app/loading'
       : 'http://localhost:3000/loading';
-  fetch(
-    `https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=${seed}&redirect_uri=${redirect_uri}&duration=permanent&scope=vote history identity read save`,
-  ).then(response => res.json({ url: response.url }));
+
+  try {
+    const response = await fetch(
+      `https://www.reddit.com/api/v1/authorize?client_id=${process.env.CLIENT_ID}&response_type=code&state=${seed}&redirect_uri=${redirect_uri}&duration=permanent&scope=vote history identity read save`,
+    );
+    return res.json({ url: response.url });
+  } catch (error) {
+    return res.json({ error });
+  }
 };
