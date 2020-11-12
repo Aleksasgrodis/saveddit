2516684ab67c0f6895d1355cd276ebdd14998582
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-11-12 16:09:47 +0100

    Adjusted custom redux persist to contain both saved & user reducers within a single localStorage key.

--- src/redux/localStorage.js
+++ src/redux/localStorage.js
@@ -1,6 +1,6 @@
 export const loadState = () => {
   try {
-    const serializedState = localStorage.getItem('saved');
+    const serializedState = localStorage.getItem('data');
     if (serializedState === null) {
       return undefined;
     }
@@ -14,7 +14,7 @@ export const loadState = () => {
 export const saveState = state => {
   try {
     const serializedState = JSON.stringify(state);
-    localStorage.setItem('saved', serializedState);
+    localStorage.setItem('data', serializedState);
   } catch (error) {
     console.log('localStorage.js error: ', error);
     return undefined;
